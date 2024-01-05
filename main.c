1dcce0f43194739a0b9d27ca25a93f55107e0544
Alex <vulga@debian-vul>
2024-01-04 20:52:33 -0800

    implemented simple chat app server side

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <netinet/in.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include "lib/socketutil.h"
+
+
+struct AcceptedSocket{
+    int acceptedSocketFD;
+    struct sockaddr_in address;
+    int error;
+    bool acceptedSuccessfully;
+};
+
+
+struct AcceptedSocket* acceptIncomingConnection(int serverSocketFD){
+    struct sockaddr_in clientAddress;
+    int clientAddressSize = sizeof(struct sockaddr_in);
+    int clientSocketFD = accept(serverSocketFD, &clientAddress, &clientAddressSize);
+    // accept is a "blocking call", after calling it it will
+    // return the "file descriptor" of the connecting client
+    // it will create a file descriptor on the server side for each connecting socket  
+    // so now our file descriptor on the server and client should be identical and we
+    // can use same functions
+
+    struct AcceptedSocket* acceptedSocket = malloc(sizeof(struct AcceptedSocket));
+    acceptedSocket->address = clientAddress;
+    acceptedSocket->acceptedSocketFD = clientSocketFD;
+    acceptedSocket->acceptedSuccessfully = clientSocketFD>0;
+
+    if (!acceptedSocket->acceptedSuccessfully){
+        acceptedSocket->error = clientSocketFD;
+    }
+
+    return acceptedSocket;
+}
 
 int main(){
 
@@ -21,20 +56,28 @@ int main(){
     int listenResult =listen(serverSocketFD, 10);
     // 10 is a "block loging ammount"(the number of connections we will queue)
         
-    struct sockaddr_in clientAddress;
-    int clientAddressSize = sizeof(struct sockaddr_in);
-    int clientSocketFD = accept(serverSocketFD, &clientAddress, &clientAddressSize);
-    // accept is a "blocking call", after calling it it will
-    // return the "file descriptor" of the connecting client
-    // it will create a file descriptor on the server side for each connecting socket  
-    // so now our file descriptor on the server and client should be identical and we
-    // can use same functions
+    struct AcceptedSocket* clientSocket = acceptIncomingConnection(serverSocketFD);
 
     char buffer[1024];
-    recv(clientSocketFD, buffer, 1024, 0);
-    // fansy read()
+    while (true) {
+        
+        size_t ammountRecv = recv(clientSocket->acceptedSocketFD, buffer, 1024, 0);
+        // fansy read()
 
-    printf("Responce: %s \n", buffer);
+        if (ammountRecv > 0){
+            buffer[ammountRecv] =0;
+            printf("Recived Responce: %s\n\r", buffer);
+        }else if(ammountRecv < 0) {
+            break;
+        }else {
+            break;
+        }
+
+    }
+
+
+    close(clientSocket->acceptedSocketFD);
+    shutdown(serverSocketFD, SHUT_RD);
 
 
     return 0;
